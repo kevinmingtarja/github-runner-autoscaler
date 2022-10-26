3ee4cbc5cc45539ac9d6e541e043058c4720d6fb
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-10-26 15:18:09 -0700

    add comments

--- runnerscaling/scaledown.go
+++ runnerscaling/scaledown.go
@@ -1,5 +1,6 @@
 package runnerscaling
 
 func (m *Manager) handleScaleDown() {
-
+	// use https://docs.github.com/en/rest/actions/self-hosted-runners#list-self-hosted-runners-for-a-repository
+	// to query runners and their status
 }
